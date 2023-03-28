6acbf4cf3d9c19f2023417a6396f1bdb1cfbf2e6
lost <danielschoolxd@gmail.com>
2023-03-28 09:09:19 +0200

    first commit

---
+++ idklolez/Ctverec.java
@@ -0,0 +1,16 @@
+public class Ctverec {
+    private int strana;
+
+    public Ctverec(int strana) {
+        this.strana = strana;
+    }
+
+    public int getStrana() {
+        return strana;
+    }
+
+    public void setStrana(int strana) {
+        this.strana = strana;
+    }
+
+}
---
+++ idklolez/ZlomekTest.java
@@ -0,0 +1,54 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZlomekTest {
+
+    @org.junit.jupiter.api.Test
+    void getCitatel() {
+        fail("Failed");
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCitatel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getJmenovatel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setJmenovatel() {
+        fail("Wrong format");
+    }
+
+    @org.junit.jupiter.api.Test
+    void scitani() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void odcitani() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nasobeni() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleni() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toString() {
+        fail("Wrong toString format");
+    }
+
+    @org.junit.jupiter.api.Test
+    void kraceni() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void NSD(int a, int b) {
+        NSD(20, 1);
+        assertEquals(10, NSD(20, 10), "Nemaji NSD");
+        return NSD;
+    }
+}
